1662d0b2af20f8ee43290f875f1f8712e03b4148
jeanyvesruffin <ruffinmax@hotmail.fr>
2020-04-20 17:11:08 +0200

    add application.properties file

--- src/main/java/com/ruffin/SpringMVCConference/SpringMvcConferenceApplication.java
+++ src/main/java/com/ruffin/SpringMVCConference/SpringMvcConferenceApplication.java
@@ -2,9 +2,10 @@ package com.ruffin.SpringMVCConference;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringMvcConferenceApplication {
+public class SpringMvcConferenceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcConferenceApplication.class, args);
