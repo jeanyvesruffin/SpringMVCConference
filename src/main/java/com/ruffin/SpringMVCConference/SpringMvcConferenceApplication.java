25d1d547a2c4117b7d8db5ef07eb32a414e11821
jeanyvesruffin <ruffinmax@hotmail.fr>
2020-04-20 10:51:34 +0200

    init projet

---
+++ src/main/java/com/ruffin/SpringMVCConference/SpringMvcConferenceApplication.java
@@ -0,0 +1,13 @@
+package com.ruffin.SpringMVCConference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcConferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcConferenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruffin/SpringMVCConference/SpringMvcConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruffin.SpringMVCConference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
